bf3155d9715c2c707d4f351d55afd886854868d8
Dusan <dusankubala1996@gmail.com>
2021-06-29 20:37:36 +0200

    Create data and base method

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
-console.log('Hello world')
+const app = new Vue({
+    el:'#App-nadpis',
+    data: {
+        title_book: 'Maly princ',
+        author: 'Juraj Kurilla',
+        created: 2000
+    },
+    methods: {
+        changeTitle() {
+            this.title_book = 'Starec a more'
+        }
+    }
+})
