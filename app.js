186ce78c7933d8532124fa2ffd85235ad10a1b8e
Dusan <dusankubala1996@gmail.com>
2021-07-05 17:21:44 +0200

    Learning bindings

--- app.js
+++ app.js
@@ -2,15 +2,17 @@ const app = new Vue({
     el:'#App-nadpis',
     data: {
         show_book: true,
-        title_book: 'Maly princ',
-        author: 'Juraj Kurilla',
-        created: 2000,
+        books: [
+            { title_book: 'Maly princ', author: 'Juraj Kurilla', created: 2000, img: 'Assets/1.jpg' },
+            { title_book: 'Maly princ', author: 'Juraj Kurilla', created: 1999, img: 'Assets/2.jpg' },
+            { title_book: 'Maly princ', author: 'Juraj Kurilla', created: 2002, img: 'Assets/3.jpg' }
+        ],
         pos_x: 0,
         pos_y: 0
     },
     methods: {
         changeTitle() {
-            this.title_book = 'Starec a more'
+            this.books[0].title_book = 'Starec a more'
         },
         hide_show_book() {
             this.show_book = !this.show_book
